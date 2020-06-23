a0e21fa02e33c8c4d7eaba5967a5938bff8ddf45
Silver Snail 57 <cdarkly1392@gmail.com>
2020-06-23 13:24:21 -0400

    Header & Wrapper components done. Src index.js done.

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+function Header () {
+    return (
+        <div className="header">
+            <h1>CS React Employee Directory</h1>
+            <p>You may click on each heading to filter OR use the search box to narrow your results</p>
+        </div>
+    )
+}
+
+export default Header;
---
+++ src/components/MainContainer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import UserContainer from "../UserContainer/index.js";
+import "./MainContainer.css";
+
+function MainContainer (){
+    return (
+        <>
+        <UserContainer />
+        </>
+    );
+};
+
+export default MainContainer;
